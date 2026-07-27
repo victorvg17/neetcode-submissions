d739844144ab77fbeafaf2d6e57ca05f9eebc75a
Victor <39340099+victorvg17@users.noreply.github.com>
2026-07-27 22:34:43 +0200

    Add: reorder-linked-list - submission-2

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-2.py
@@ -0,0 +1,88 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head:
+            return None
+
+        # Brute force approach
+        # nodes = []
+        # curr = head
+        # while curr:
+        #     nodes.append(curr)
+        #     curr = curr.next
+
+        # i, j = 0, len(nodes) - 1
+        # while i < j:
+        #     nodes[i].next = nodes[j]
+        #     i += 1
+        #     if i >= j:
+        #         break
+        #     nodes[j].next = nodes[i]
+        #     j -= 1
+        
+        # nodes[i].next = None
+
+        # Reverse and merge approach
+        # slow, fast = head, head.next
+        # while fast and fast.next:
+        #     slow = slow.next
+        #     fast = fast.next.next
+
+        # second = slow.next
+        # prev = slow.next = None
+        # while second:
+        #     tmp = second.next
+        #     second.next = prev
+        #     prev = second
+        #     second = tmp
+
+        # first, second = head, prev
+        # while second:
+        #     tmp1, tmp2 = first.next, second.next
+        #     first.next = second
+        #     second.next = tmp1
+
+        #     first, second = tmp1, tmp2
+        # nodes = []
+        # curr = head
+        # while curr:
+        #     nodes.append(curr)
+        #     curr = curr.next
+        
+        # i, j = 0, len(nodes) - 1
+        # while i < j:
+        #     nodes[i].next = nodes[j]
+        #     i += 1
+        #     if i >= j:
+        #         break
+        #     nodes[j].next = nodes[i]
+        #     j -= 1
+        # nodes[i].next = None
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        curr1 = slow.next
+        prev = slow.next = None
+        while curr1:
+            tmp = curr1.next
+            curr1.next = prev
+            prev = curr1
+            curr1 = tmp
+
+        first, second = head, prev
+        while second:
+            tmp1 = first.next
+            tmp2 = second.next
+            first.next = second
+            second.next = tmp1
+
+            first = tmp1
+            second = tmp2
+
